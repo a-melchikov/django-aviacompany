edaec2e607241d9f9dd0999ed75490057af88d71
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-06-26 00:17:00 +0400

    Minor adjustments

--- users/views.py
+++ users/views.py
@@ -6,9 +6,9 @@ from .models import CustomUser
 
 def user_login(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "Вы успешно вошли в систему")
